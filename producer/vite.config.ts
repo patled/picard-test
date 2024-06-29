466ff0bb462b09ed1de751d8105e2e72a0e4a491
patled <github@lederer.cloud>
2024-06-29 14:11:24 +0200

    chore: Update consumer index.html and producer configuration
    This commit updates the consumer index.html file to use a different source for the pi-component, pointing to http://localhost:6789/assets/remoteEntry.js. It also updates the producer's vite.config.ts file to expose the 'App' module instead of './App'. These changes were made to align with the updated project requirements and improve the integration between the consumer and producer components.

--- producer/vite.config.ts
+++ producer/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       name: 'producer',
       filename: 'remoteEntry.js',
       exposes: {
-        './App': './src/App',
+        'App': './src/App',
       },
     })],
   
